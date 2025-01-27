3fe0f592cb054851e0e9cf592254e65c1834f4ad
adityashekhar740 <shekharaditya740@gmail.com>
2025-01-27 23:16:37 +0530

    index.js

--- Api/index.js
+++ Api/index.js
@@ -12,14 +12,14 @@ const cookieParser=require('cookie-parser');
 const PrivateRouter =require('./routes/PrivateRoute');
 const app=express();
 dotenv.config();
-const corsOptions={
-    origin:'http://localhost:5173',
-    methods:'GET,POST',
-    Credentials:true,
-}
+// const corsOptions={
+//     origin:'http://localhost:5173',
+//     methods:'GET,POST',
+//     Credentials:true,
+// }
 
 app.use(cookieParser());
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());                // to send data in json form
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("Connected to MongoDB");
